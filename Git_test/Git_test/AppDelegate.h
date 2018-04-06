f7d6f39d7b2ea0b7eef18668cc2760762f8b098e
Dmitriy <dima_tarelkin@mail.ru>
2018-04-06 15:19:38 +0300

    Please enter the commit message for your changes. Lines starting :wq

---
+++ Git_test/Git_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git_test
+//
+//  Created by Dmitriy Tarelkin on 6/4/18.
+//  Copyright © 2018 Dmitriy Tarelkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git_test/Git_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git_test
+//
+//  Created by Dmitriy Tarelkin on 6/4/18.
+//  Copyright © 2018 Dmitriy Tarelkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
